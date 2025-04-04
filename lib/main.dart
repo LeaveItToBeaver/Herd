91992a03f3e18fb52870b577fb9c90894159b215
leaveittobeaver <jasonbeaverw99@gmail.com>
2025-04-04 16:33:11 -0400

    Feat: Enhance alt feed repository with batch querying, hot score sorting, and herd ID integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'dart:async';
+
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:herdapp/core/utils/router.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  unawaited(MobileAds.instance.initialize());
   await Firebase.initializeApp();
   await FirebaseAppCheck.instance.activate(
     webProvider: ReCaptchaV3Provider('recaptcha-v3-site-key'),
