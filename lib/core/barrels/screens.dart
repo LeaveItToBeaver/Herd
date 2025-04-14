f5786366b2da8be9ae0fad6305d7208aa4010abe
leaveittobeaver <jasonbeaverw99@gmail.com>
2025-04-14 07:27:20 -0400

    Feat: Add settings screen with user preferences management, including NSFW content controls and cache settings

--- lib/core/barrels/screens.dart
+++ lib/core/barrels/screens.dart
@@ -1,31 +1,25 @@
 //Auth
 export 'package:herdapp/features/auth/view/screens/login_screen.dart';
 export 'package:herdapp/features/auth/view/screens/signup_screen.dart';
-
-//User
-export 'package:herdapp/features/edit_user/view/screens/edit_profile_screen.dart';
+//Comment
+export 'package:herdapp/features/comment/view/screens/comment_thread_screen.dart';
 export 'package:herdapp/features/edit_user/alt_profile/view/screens/edit_alt_profile_screen.dart';
 export 'package:herdapp/features/edit_user/public_profile/view/screens/edit_public_profile_screen.dart';
-export 'package:herdapp/features/user/view/screens/alt_profile_screen.dart';
-export 'package:herdapp/features/user/view/screens/public_profile_screen.dart';
-export 'package:herdapp/features/user/view/screens/connection_request_screen.dart';
-
+//User
+export 'package:herdapp/features/edit_user/view/screens/edit_profile_screen.dart';
 //Feed
 export 'package:herdapp/features/feed/alt_feed/view/screens/alt_screen.dart';
 export 'package:herdapp/features/feed/public_feed/view/screens/public_screen.dart';
-
-//Post
-export 'package:herdapp/features/post/view/screens/post_screen.dart';
-export 'package:herdapp/features/post/view/screens/create_post_screen.dart';
-
 //Notifications
 export 'package:herdapp/features/notifications/view/screens/notification_screen.dart';
-
+export 'package:herdapp/features/post/view/screens/create_post_screen.dart';
+//Post
+export 'package:herdapp/features/post/view/screens/post_screen.dart';
 //Search
 export 'package:herdapp/features/search/view/screens/search_screen.dart';
-
+// Settings
+export 'package:herdapp/features/user/view/screens/alt_profile_screen.dart';
+export 'package:herdapp/features/user/view/screens/connection_request_screen.dart';
+export 'package:herdapp/features/user/view/screens/public_profile_screen.dart';
 //Misc
 export 'package:herdapp/screens/splash_screen.dart';
-
-//Comment
-export 'package:herdapp/features/comment/view/screens/comment_thread_screen.dart';
